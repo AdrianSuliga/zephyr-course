067ccaa528d680d855ac9b8b77bbe977b671504b
Adrian Suliga <adriansuliga81@gmail.com>
2026-05-21 20:17:45 +0200

    Add our_driver extension API to shell commands for L7

--- app/drivers/our_driver/our_driver_shell.c
+++ app/drivers/our_driver/our_driver_shell.c
@@ -1,5 +1,6 @@
 #include <zephyr/shell/shell.h>
 #include <zephyr/drivers/sensor.h>
+#include "our_driver_extension.h"
 
 static int cmd_sample_fetch_handler(const struct shell *sh, int argc, char **argv)
 {
@@ -50,10 +51,36 @@ static int cmd_info_handler(const struct shell *sh, int argc, char **argv)
     return 0;
 }
 
+static int cmd_set_handler(const struct shell *sh, int argc, char **argv)
+{
+    const struct device *driver = shell_device_get_binding(argv[1]);
+    if (!driver) {
+        shell_error(sh, "Could not find device %s", argv[1]);
+        return -EFAULT;
+    }
+
+    int ret;
+    uint32_t new_calibration_value = atoi(argv[2]);
+    
+    if (0 <= new_calibration_value && new_calibration_value <= 3000) {
+        ret = modify_device_instance(driver, new_calibration_value);
+        if (ret != 0) {
+            shell_error(sh, "Failed to set %s calibration value to %u", argv[1], new_calibration_value);
+            return ret;
+        }
+
+        return 0;
+    } else {
+        shell_error(sh, "Calibration value outside of range (0 - 3000)");
+        return -ERANGE;
+    }
+}
+
 SHELL_STATIC_SUBCMD_SET_CREATE(our_driver_subcmds,
-    SHELL_CMD_ARG(fetch, NULL, "Turn LED ON", cmd_sample_fetch_handler, 2, 0),
-    SHELL_CMD_ARG(read, NULL, "Turn LED OFF", cmd_channel_get_handler, 2, 0),
-    SHELL_CMD_ARG(info, NULL, "Get driver name and readiness", cmd_info_handler, 2, 0),
+    SHELL_CMD_ARG(fetch, NULL, "Turn LED ON\nUsage: fetch <driver_name>", cmd_sample_fetch_handler, 2, 0),
+    SHELL_CMD_ARG(read, NULL, "Turn LED OFF\nUsage: read <driver_name>", cmd_channel_get_handler, 2, 0),
+    SHELL_CMD_ARG(info, NULL, "Get driver name and readiness\nUsage: info <driver_name>", cmd_info_handler, 2, 0),
+    SHELL_CMD_ARG(set, NULL, "Set sensor calibration value (0 - 3000)\n Usage: set <driver_name> <value>", cmd_set_handler, 3, 0),
     SHELL_SUBCMD_SET_END,
 );
 
